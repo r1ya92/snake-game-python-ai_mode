f733de85a2b9d1567c145f051642f7550675834b
R1ya-hub <2024.riya.bhatt@ves.ac.in>
2026-07-03 12:59:58 +0530

    Added "High Score" function

--- scoreboard.py
+++ scoreboard.py
@@ -5,6 +5,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.penup()
         self.hideturtle()
         self.goto(0, 270)
@@ -13,7 +15,16 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.clear()
+        self.write(f"Score: {self.score}  High Score: {self.high_score}", align="center", font=("Arial", 24, "normal"))
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt",mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
 
     def game_over(self):
         self.goto(0, 0)
